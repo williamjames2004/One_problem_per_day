d618e776f9f2015f56b3102a935ddeb44d556741
WILLIAM JAMES <161729235+williamjames2004@users.noreply.github.com>
2026-01-02 20:34:04 +0530

    Update userReport.js

--- models/userReport.js
+++ models/userReport.js
@@ -30,7 +30,39 @@ const UserReportSchema = new mongoose.Schema({
         max: 100
       }
     }
+  ],
+
+  debug: [
+    {
+      program_id: {
+        type: String,
+        required: true
+      },
+      total_score: {
+        type: Number,
+        required: true,
+        min: 0
+      },
+      obtained_score: {
+        type: Number,
+        required: true,
+        min: 0
+      },
+      percentage: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 100
+      },
+      errors: [
+        {
+          testCase: Number,
+          message: String
+        }
+      ]
+    }
   ]
+
 }, { timestamps: true });
 
 module.exports = mongoose.model("UserReport", UserReportSchema);
