7ee78f0754048101296c651d72377c43d21f00e1
WILLIAM JAMES <161729235+williamjames2004@users.noreply.github.com>
2026-01-04 20:21:01 +0530

    Add files via upload

---
+++ models/Admin.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const adminSchema = new mongoose.Schema(
+  {
+    admin_code: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+/* Hash password before saving */
+adminSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+module.exports = mongoose.model("Admin", adminSchema);
