bb3df67ac9431df6ed71aaa5c6a25ed43b75d00e
WILLIAM JAMES <161729235+williamjames2004@users.noreply.github.com>
2026-07-07 09:45:32 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Staff = require("../models/Staff");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 
 // ================= REGISTER =================
